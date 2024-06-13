e7b436ad2f4608003e76df933dbefc01ae574b5e
TanjaTh <tanja.thomaser@additive.eu>
2024-06-13 18:05:08 +0200

    injected forecast HTML in js

--- src/index.js
+++ src/index.js
@@ -58,7 +58,39 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="row">
+        <div class="col-2">
+          <div class="weather-forecast-date">${day}</div>
+
+          <img
+            src="https://cdn.search.brave.com/serp/v2/_app/immutable/assets/10d.Cpfalt5N.svg"
+            alt=""
+            width="48"
+          />
+
+          <div class="weather-forecast-temperature">
+            <span class="weather-forecast-temperature-max"><strong>18°</strong></span>
+            <span class="weather-forecast-temperature-min">12°</span>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Bolzano");
+displayForecast();
